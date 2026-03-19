6594d75681492134222f190c77e30c8ce78aec95
matheusmaiarangel <matheusmaiarangel@gmail.com>
2026-03-19 14:21:40 -0300

    Criação da struct e definição base de atributos

---
+++ Models/Projeto.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercicioProjeto.Models;
+
+struct Projeto
+{
+    public int Id;
+    public string NomeProj;
+    public string NomeAluno;
+    public string Area;
+    public int Semestre;
+    public string Status;
+}
+
